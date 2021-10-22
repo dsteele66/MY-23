8e7965247e30a0ee4c9c3a4ab6349e89cb52aa8b
Duncan Steele <81034105+dsteele66@users.noreply.github.com>
2021-10-22 14:38:52 -0400

    First Commit

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TwentythreeConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'twentythree'
---
+++ settings.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE twentythree;
+CREATE USER twentythreeuser WITH PASSWORD 'twentythree';
+GRANT ALL PRIVILEGES ON DATABASE twentythree TO twentythreeuser;
